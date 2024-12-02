84dd1fc091f17f45a2793317a757dfc70d90f465
arnulfo <arnulfoaceved0215@outlook.com>
2024-12-03 08:48:19 +0900

    socket upr

--- lib/socket.ts
+++ lib/socket.ts
@@ -4,7 +4,7 @@ let socket: SocketIOClient.Socket | null = null;
 
 const useSocket = () => {
   if (!socket) {
-    socket = socketClient('https://islandhousetest.com');
+    socket = socketClient('https://localhost:3000');
   }
 
   return { socket };
