d210a1f97ef4063a5049e6f7b8bd4dd646f7fbe5
Vimal Bhalani <vimalbhalani77@gmail.com>
2024-12-10 11:06:52 +0530

    use server.js for production

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,5 +1,4 @@
 import NextAuth from 'next-auth';
 import authConfig from './auth.config';
 
-console.log('Auth env', process.env);
 export const { auth, handlers, signOut, signIn } = NextAuth(authConfig);
